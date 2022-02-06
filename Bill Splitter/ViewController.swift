db626b8ba9394ed27c6100737de2fb48b41b2946
Gustavo Ziger <gustavoziger@gmail.com>
2022-02-06 16:47:42 -0300

    Initial Commit

---
+++ Bill Splitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill Splitter
+//
+//  Created by Gustavo Ziger on 06/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
